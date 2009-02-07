c8e5b86cc0758476a554808b075c47820161fa18
Teflon Ted <github@rudiment.net>
2009-02-06 22:28:15 -0500

    fixed date compare (was doing string compare)

--- lib/twitter_monkey_patch.rb
+++ lib/twitter_monkey_patch.rb
@@ -3,7 +3,7 @@ module Twitter
     def merged_timeline(options={})
       tl = timeline(:friends, options)
       dm = direct_messages(options)
-      (tl+dm).sort{|a,b|a.created_at <=> b.created_at}
+      (tl+dm).sort{|a,b|Date.parse(a.created_at) <=> Date.parse(b.created_at)}
     end
   end
 end
--- test/unit/timeline_test.rb
+++ test/unit/timeline_test.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TimelineTest < ActiveSupport::TestCase
   def test_merged_timeline
-    t = Twitter::Base.new('teflonted', 'donthackme')
-    assert t.timeline
-    assert t.direct_messages
-    assert t.merged_timeline
+    twitter = Twitter::Base.new('teflonted', 'donthackme')
+    tweets = twitter.merged_timeline
+    p tweets.first
+    p tweets.last
   end
 end
