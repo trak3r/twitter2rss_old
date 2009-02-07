b76565340a4a38a519f67cc8cd4f88110000c0cb
Teflon Ted <github@rudiment.net>
2009-02-07 10:21:34 -0500

    scaffold'd tweet controller

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,5 @@
+class TweetController < ApplicationController
+  def index
+  end
+
+end
---
+++ test/functional/tweet_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class TweetControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
