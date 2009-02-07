4f93ec719c5d9a3c3c055459e933aa22e9acb8b0
Teflon Ted <github@rudiment.net>
2009-02-07 10:52:32 -0500

    removed password from unit test - note to self: change password soon

--- test/unit/timeline_test.rb
+++ test/unit/timeline_test.rb
@@ -1,10 +1,5 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TimelineTest < ActiveSupport::TestCase
-  def test_merged_timeline
-    twitter = Twitter::Base.new('teflonted', 'donthackme')
-    tweets = twitter.merged_timeline
-    p tweets.first
-    p tweets.last
-  end
+
 end
