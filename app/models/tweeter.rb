77c83f1052ff4f61ebebef9cdf0d822a79c909aa
Teflon Ted <github@rudiment.net>
2009-02-07 21:20:12 -0500

    fixed last_polled_at nil issue

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -4,7 +4,12 @@ class Tweeter < ActiveRecord::Base
 
   def tweets(password)
     twitter = Twitter::Base.new(self.screen_name, password)
-    _tweets = twitter.merged_timeline(:since => self.last_polled_at)
+    if self.last_polled_at
+      options = {:since => self.last_polled_at}
+    else
+      options = {}
+    end
+    _tweets = twitter.merged_timeline(options)
     if _tweets
       if _tweets.last
         self.last_polled_at = Date.parse(_tweets.last.created_at)
