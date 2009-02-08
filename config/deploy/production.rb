d8b1426ed44c13659c314b662697e2df3806a113
Teflon Ted <github@rudiment.net>
2009-02-08 16:05:21 -0500

    changed deployment to link db instead of including db password in repository

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -39,37 +39,19 @@ set :deploy_via, :remote_cache
 #	Passenger
 #############################################################
 
+after "deploy:update_code", "db:symlink"
+
+namespace :db do
+  desc "Make symlink for database yaml"
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
 namespace :deploy do
 
   after 'deploy', 'deploy:migrate'
   after 'deploy:migrate', 'deploy:cleanup'
-
-  desc "Create the database yaml file"
-  task :after_update_code do
-    db_config = <<-EOF
-    production:
-      adapter: mysql
-      encoding: utf8
-      username: trak3r
-      password: h0tf0rw0rds
-      database: tweeters
-      host: mysql.anachromystic.com
-    EOF
-
-    put db_config, "#{release_path}/config/database.yml"
-
-    #########################################################
-    # Uncomment the following to symlink an uploads directory.
-    # Just change the paths to whatever you need.
-    #########################################################
-
-    # desc "Symlink the upload directories"
-    # task :before_symlink do
-    #   run "mkdir -p #{shared_path}/uploads"
-    #   run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
-    # end
-
-  end
 
   # Restart passenger on deploy
   desc "Restarting mod_rails with restart.txt"
