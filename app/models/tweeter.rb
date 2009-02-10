34d33a6c4cf3d50c1ba2a103b69112fb914d6454
Teflon Ted <github@rudiment.net>
2009-02-10 15:22:13 -0500

    switched back to :count, :since wasn't working as expected and too lazy right now to figure it out

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -4,7 +4,7 @@ class Tweeter < ActiveRecord::Base
 
   def tweets(password)
     twitter = Twitter::Base.new(self.screen_name, password)
-    _tweets = twitter.merged_timeline(:since => 1.days.ago)
+    _tweets = twitter.merged_timeline(:count => 100)
     update_attribute(:last_polled_at, Time.now) if (_tweets && _tweets.last)
     _tweets
   end
