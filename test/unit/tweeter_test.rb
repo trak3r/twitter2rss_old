60a431018bbb9982373efe5005bf1dc5be5750cd
Teflon Ted <github@rudiment.net>
2009-02-15 19:16:20 -0500

    increased screen name and password lengths to ensure 512 is enough encryption bits

--- test/unit/tweeter_test.rb
+++ test/unit/tweeter_test.rb
@@ -3,10 +3,13 @@ require 'test_helper'
 class TweeterTest < ActiveSupport::TestCase
 
   test "junk and stuff" do
-    token = Tweeter.encode('teflonted', 'donthackme')
-    screen_name, password = Tweeter.decode token
-    assert_equal screen_name, 'teflonted'
-    assert_equal password, 'donthackme'
+    plain_screen_name = 'fifteencharctrs'
+    plain_password = 'howlongistoolong'
+    token = Tweeter.encode(plain_screen_name, plain_password)
+    puts "http://twitter2rss.anachromystic.com/#{token}.rss"
+    decoded_screen_name, decoded_password = Tweeter.decode token
+    assert_equal plain_screen_name, decoded_screen_name
+    assert_equal plain_password, decoded_password
   end
   
 end
