226fdc7bd67fd333c765778b516886222e3c4f2d
Thomas Teflon Ted Davis <github@rudiment.net>
2009-03-07 19:58:44 -0500

    filter search results of dupes from people already being followed

--- lib/twitter_monkey_patch.rb
+++ lib/twitter_monkey_patch.rb
@@ -3,21 +3,36 @@ module Twitter
     
     def merged_timeline(options={})
       tl = timeline(:friends, options)
+      note_followings(tl)
       dm = direct_messages(options)
       rf = references
       (tl+dm+rf).sort{|b,a|Date.parse(a.created_at) <=> Date.parse(b.created_at)}
     end
-
+    
     def references
       filtered = []
       Twitter::Search.new("@#{screen_name}").per_page(100).each do |result|
-        # don't include @replies
-        filtered << result unless result.text.starts_with?("@#{screen_name}")
+        filtered << result unless(is_reply?(result) || already_following?(result))
       end
       filtered
     end
 
     private
+
+    def note_followings(tweets)
+      tweets.each do |tweet|
+        @followings ||= []
+        @followings << tweet.user.screen_name
+      end
+    end
+    
+    def already_following?(result)
+      @followings.include?(result.from_user)
+    end
+    
+    def is_reply?(result)
+      result.text.starts_with?("@#{screen_name}")
+    end
 
     def screen_name
       @config[:email]
