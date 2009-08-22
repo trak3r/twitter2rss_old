87fa1e0093ed2c9b7eeee9e0a997c343d1c63838
Thomas 'Ted' Davis <github@rudiment.net>
2009-08-22 16:37:58 -0400

    vendor'd crack

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'crack', :version => '0.1.4'
   config.gem 'httparty', :version => '0.4.4'
   config.gem 'twitter', :version => '0.4.1'
 
