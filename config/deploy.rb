0a5890e48094fafa21c1f02759bc4b7eca77fe60
Thomas 'Ted' Davis <github@rudiment.net>
2009-08-29 20:20:17 -0400

    reduced number of entries pulled to speed-up feed generation

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
 set :stages, %w(staging production)
 set :default_stage, "production"
-require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+# require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+require 'capistrano/ext/multistage'
--- lib/twitter_monkey_patch.rb
+++ lib/twitter_monkey_patch.rb
@@ -7,15 +7,24 @@ module Twitter
     end  
     
     def merged_timeline(options={})
-      tl = timeline(:friends, {:count => 99}.merge(options))
-      dm = direct_messages({:count => 22}.merge(options))
-      rf = references
+      tl = []
+      Tweeter.benchmark "timeline" do
+        tl = timeline(:friends, {:count => 44}.merge(options))
+      end
+      dm = []
+      Tweeter.benchmark "direct_messages" do
+        dm = direct_messages({:count => 22}.merge(options))
+      end
+      rf = []
+      Tweeter.benchmark "references" do
+        rf = references
+      end
       (tl+dm+rf).sort{|b,a|Date.parse(a.created_at) <=> Date.parse(b.created_at)}
     end
     
     def references
       filtered = []
-      Twitter::Search.new("@#{screen_name}").per_page(100).each do |result|
+      Twitter::Search.new("@#{screen_name}").per_page(22).each do |result|
         filtered << result unless(is_reply?(result) || already_following?(result))
       end
       filtered[0..20]
