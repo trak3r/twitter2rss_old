a059915e3a8cc71aad5d50230ad87ae81b0fdd11
Thomas Davis <ted@ceylon.local>
2009-12-08 18:43:04 -0500

    catch and dump exceptions to page as work-around for heroku logs not showing the top half (most important part) of the stach trace

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,5 +2,7 @@ class TweetController < ApplicationController
   def index
     @tweeter = Tweeter.find_or_create_by_screen_name(params[:screen_name])
     @twitter = Twitter::Base.new(@tweeter.screen_name, params[:password])
+  rescue Exception => e
+    render :text => e.backtrace.join('<br/>')
   end
 end
