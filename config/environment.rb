960795c9ba1e74d5ff903d5ad795ab75f96866ad
Thomas Davis <ted@ceylon.local>
2009-12-12 10:30:08 -0500

    unpacked the crack gem

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  # config.gem 'crack', :version => '0.1.4'
+  config.gem 'crack', :version => '0.1.4'
   config.gem 'httparty', :version => '0.4.3'
   config.gem 'hashie', :version => '0.1.5'
   config.gem 'ruby-hmac', :lib => 'hmac', :version => '0.3.2'
